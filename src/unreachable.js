2d81625a7d59eea720a9f880256d91635f0e78a2
Irakli Gozalishvili <rfobic@gmail.com>
2017-08-03 16:53:58 -0700

    Fix errors.

--- src/unreachable.js
+++ src/unreachable.js
@@ -4,3 +4,5 @@ export const unreachable = (value: empty): empty => {
   const format = JSON.stringify(value)
   throw new TypeError(`Internal error: Encountered impossible value: ${format}`)
 }
+
+export default unreachable
